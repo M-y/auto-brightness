202a3e23686709a1a41732992538cace3ca22194
muhammed <ben@muhammed.im>
2020-05-26 18:20:25 +0300

    webcam module

---
+++ webcam.py
@@ -0,0 +1,25 @@
+import cv2
+
+class Camera:
+    camera = '/dev/video0'
+    
+    def __init__(self, camera):
+        self.camera = camera
+    
+    def getImage(self):
+        cam = cv2.VideoCapture(self.camera)
+        ret, frame = cam.read()
+        cam.release()
+        return ret, frame
+    
+    def __getBrightness(self, image):
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mean = cv2.mean(hsv)
+        brightness = mean[2]
+        return brightness
+    
+    def getBrightness(self):
+        ret, image = self.getImage()
+        if ret:
+            return self.__getBrightness(image)
+        return 0
