97768b7d9741302737112f64afa098cd53144a4e
muhammed <ben@muhammed.im>
2020-06-02 23:19:52 +0300

    create deb script version parameter

--- createdeb.sh
+++ createdeb.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-mkdir --parents autobrightness-0.1.0/tmp/autobrightness-0.1.0
-find . -mindepth 1 -maxdepth 1 ! -regex '\(./.git\|./.gitignore\|./.vscode\|./.github\|./createdeb.sh\|./autobrightness-0.1.0\)' -exec cp -r {} autobrightness-0.1.0/tmp/autobrightness-0.1.0 \;
-mkdir autobrightness-0.1.0/DEBIAN
+VER=$1
+mkdir --parents "deb/tmp/autobrightness-$VER"
+find . -mindepth 1 -maxdepth 1 ! -regex '\(./.git\|./.gitignore\|./.vscode\|./.github\|./createdeb.sh\|./deb\)' -exec cp -r {} "deb/tmp/autobrightness-$VER" \;
+mkdir deb/DEBIAN
 echo "Package: autobrightness
-Version: 0.1.0
+Version: $VER
 Architecture: amd64
 Depends: python3-pip
 Maintainer: Muhammed YILDIRIM <ben@muhammed.im>
-Description: Auto change screen brightness using webcam" > autobrightness-0.1.0/DEBIAN/control
+Description: Auto change screen brightness using webcam" > deb/DEBIAN/control
 echo "#!/bin/bash
-pip install /tmp/autobrightness-0.1.0
-rm -rf /tmp/autobrightness-0.1.0" > autobrightness-0.1.0/DEBIAN/postinst
-chmod +x autobrightness-0.1.0/DEBIAN/postinst
-dpkg-deb --build autobrightness-0.1.0
-rm -rf autobrightness-0.1.0
+pip install /tmp/autobrightness-$VER
+rm -rf /tmp/autobrightness-$VER" > deb/DEBIAN/postinst
+chmod +x deb/DEBIAN/postinst
+dpkg-deb --build deb "autobrightness-$VER.deb"
+rm -rf deb
