3f88d9169fdefe0b0f33aa7da102b9fee71660c1
muhammed <ben@muhammed.im>
2020-06-03 23:06:39 +0300

    use os module in sysfs backend

--- autobrightness/backend/sysfs.py
+++ autobrightness/backend/sysfs.py
@@ -1,18 +1,28 @@
+import os
+import sys
+
 class sysfs:
+    def __init__(self):
+        for (dirpath, dirname, file) in os.walk("/sys/class/backlight"):
+            self.sysfs_dir = os.path.join(dirpath, dirname[0])
+        
+        if not os.access(os.path.join(self.sysfs_dir, "brightness"), os.W_OK):
+            sys.exit(self.sysfs_dir + " is not writable!")
+
     def getMaxBrightness(self):
-        file = open("/sys/class/backlight/intel_backlight/max_brightness", "r")
+        file = open(os.path.join(self.sysfs_dir, "max_brightness"), "r")
         maxBrightness = int( file.read() )
         file.close()
         return maxBrightness
     
     def getBrightness(self):
-        file = open("/sys/class/backlight/intel_backlight/actual_brightness", "r")
+        file = open(os.path.join(self.sysfs_dir, "actual_brightness"), "r")
         brightness = int( file.read() )
         file.close()
         return brightness
     
     def setBrightness(self, val):
-        file = open("/sys/class/backlight/intel_backlight/brightness", "w")
+        file = open(os.path.join(self.sysfs_dir, "brightness"), "w")
         file.write( str(val) )
         file.close()
         
