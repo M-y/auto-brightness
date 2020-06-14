d381d105b35c274cb0a9de8ab6f373dd5c153e50
M-y <ben@muhammed.im>
2020-06-14 13:35:40 +0300

    powercfg subprocess encoding fix in windowed mode

--- autobrightness/__init__.py
+++ autobrightness/__init__.py
@@ -1,4 +1,4 @@
 import os
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
--- autobrightness/backend/powercfg.py
+++ autobrightness/backend/powercfg.py
@@ -12,8 +12,40 @@ class Powercfg():
     def __init__(self, lang):
         global _
         _ = lang.gettext
-
+        
+        self.cp = self._getcodepage()
         self.guid = self._subprocess("Power Setting GUID: (.*?) ")
+
+    def _subprocess_args(self, encoding = None):
+        """
+        Builds and returns extra arguments for subprocess
+        """
+
+        si = subprocess.STARTUPINFO()
+        si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        args = {    
+            'stdin': subprocess.PIPE,
+            'stderr': subprocess.PIPE,
+            'startupinfo': si,
+            'shell':True,
+            'env': os.environ
+        }
+
+        if not encoding is None:
+            args.update({'encoding':encoding})
+
+        return args
+
+    def _getcodepage(self):
+        """
+        Returns active code page
+        """
+
+        cp = subprocess.check_output("chcp", **self._subprocess_args())
+        cp = re.search("Active code page: (.*?)$", cp.decode())
+        cp = cp.group(1)
+
+        return "cp" + cp
 
     def _subprocess(self, regex):
         """
@@ -25,11 +57,10 @@ class Powercfg():
         Returns:
             string: regex group 1
         """
-
-        result = subprocess.Popen(["POWERCFG", "/Q"], stdout=subprocess.PIPE, encoding=os.device_encoding(0))
+        result = subprocess.check_output(["POWERCFG", "/Q"], **self._subprocess_args(self.cp))
         foundBrightnessString = False
-
-        for line in result.stdout.readlines():
+        
+        for line in result.split("\n"):
             line = line.rstrip()
             
             # search for display brightness sub group
@@ -49,4 +80,4 @@ class Powercfg():
 
     def setBrightness(self, val):
         subprocess.call(["POWERCFG", "/SETACVALUEINDEX", "SCHEME_CURRENT", "SUB_VIDEO", self.guid, str(val)])
-        print("/SETACVALUEINDEX SCHEME_CURRENT SUB_VIDEO " + self.guid + " " + str(val))
+        subprocess.call(["POWERCFG", "/S", "SCHEME_CURRENT"])
