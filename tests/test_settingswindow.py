e890895c91eacadfa6537384420f4710a9abf9a6
muhammed <ben@muhammed.im>
2020-06-25 23:34:21 +0300

    settings window unittest

---
+++ tests/test_settingswindow.py
@@ -0,0 +1,54 @@
+import unittest
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+from autobrightness import config
+from autobrightness.gui import settingscontroller, settingswindow, daemon
+import gettext
+
+app = QApplication([])
+
+class SettingswindowTest(unittest.TestCase):
+    def createWindow(self, configIns):
+        service = daemon.Service()
+        self.view = settingswindow.SettingsWindow(gettext)
+        settingscontroller.SettingsController(self.view, configIns, service, gettext)
+
+    def checkConfig(self, configIns):
+        """
+        Checks if settings on window equals config instance
+        """
+        self.assertEqual(self.view.languageCombo.currentText(), configIns.language)
+        self.assertEqual(self.view.backendCombo.currentText(), configIns.backend)
+        self.assertEqual(self.view.cameraEdit.text(), str(configIns.camera))
+        self.assertEqual(self.view.intervalEdit.text(), str(configIns.interval))
+        self.assertEqual(self.view.shortcutEdit.text(), str(configIns.shortcut))
+    
+    def comboChange(self, comboBox):
+        """
+        Loops through all items in the combo box and change to all of them
+        """
+        for i in range( comboBox.count() ):
+            if i != comboBox.currentIndex():
+                comboBox.setCurrentIndex(i)
+
+    def test_defaultConfig(self):
+        self.createWindow( config.Config() )
+        self.checkConfig( config.Config() )
+    
+    def test_saveButton(self):
+        configIns = config.Config()
+        self.createWindow(configIns)
+        
+        # change settings on window
+        self.comboChange(self.view.languageCombo)
+        self.comboChange(self.view.backendCombo)
+        self.view.cameraEdit.setText("1")
+        self.view.intervalEdit.setText("1")
+        self.view.shortcutEdit.setText("F12")
+
+        # click save and check config
+        QTest.mouseClick(self.view.saveButton, Qt.LeftButton)
+        configIns.load()
+        self.checkConfig(configIns)
+
