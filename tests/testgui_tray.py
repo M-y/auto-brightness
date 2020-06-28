f216be27987c12f4e04f92f56485ea9077ac2e2b
muhammed <ben@muhammed.im>
2020-06-28 14:51:27 +0300

    tray icon unittest fix

--- tests/testgui_tray.py
+++ tests/testgui_tray.py
@@ -6,29 +6,32 @@ from autobrightness import config
 from autobrightness.gui import trayicon, daemon, settingswindow, logwindow
 import gettext
 
-app = QApplication([])
 
 class TrayiconTest(unittest.TestCase):
-    # def test_settingsaction(self):
-    #     trayIcon = trayicon.TrayIcon(config.Config(), daemon.Service(), app, gettext)
-    #     for action in trayIcon.contextMenu().actions():
-    #         if action.objectName() == "settings":
-    #             action.trigger()
-        
-    #     view = None
-    #     for widget in app.allWidgets():
-    #         if type(widget) == settingswindow.SettingsWindow:
-    #             view = widget
-    #     self.assertIsInstance(view, settingswindow.SettingsWindow)
+    def initTrayIcon(self):
+        self.app = QApplication([])
+        self.trayIcon = trayicon.TrayIcon(config.Config(), daemon.Service(), self.app, gettext)
+    
+    def triggerMenuAction(self, actionName):
+        for action in self.trayIcon.contextMenu().actions():
+            if action.objectName() == actionName:
+                action.trigger()
+
+    def findWidget(self, widgetType):
+        view = None
+        for widget in self.app.allWidgets():
+            if type(widget) == widgetType:
+                view = widget
+        return view
+
+    def test_settingsaction(self):
+        self.initTrayIcon()
+        self.triggerMenuAction("settings")
+        view = self.findWidget(settingswindow.SettingsWindow)
+        self.assertIsInstance(view, settingswindow.SettingsWindow)
     
     def test_logsaction(self):
-        trayIcon = trayicon.TrayIcon(config.Config(), daemon.Service(), app, gettext)
-        for action in trayIcon.contextMenu().actions():
-            if action.objectName() == "logs":
-                action.trigger()
-        
-        view = None
-        for widget in app.allWidgets():
-            if type(widget) == logwindow.LogWindow:
-                view = widget
+        self.initTrayIcon()
+        self.triggerMenuAction("logs")
+        view = self.findWidget(logwindow.LogWindow)
         self.assertIsInstance(view, logwindow.LogWindow)
