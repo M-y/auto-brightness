71267be88926d847efebf9c8ff567a7dba6f46bf
muhammed <ben@muhammed.im>
2020-07-18 14:12:18 +0300

    coverage script fix

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mv -f ~/.autobrightness ~/.autobrightness.backup
 coverage erase
-coverage run --source autobrightness -m unittest discover -s tests
+coverage run --source autobrightness -m unittest discover -s tests -v -f
 coverage xml -i
 mv -f ~/.autobrightness.backup ~/.autobrightness
